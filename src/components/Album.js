6911953d59f125700970a18705549e8d60262560
Raymond Woods <ray@MacBook-Pro.local>
2018-08-11 14:28:53 -0700

    checkpoint 5: * Adding album data * Filling in library content * Linking to albums

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 class Album extends Component {
   render() {
     return (
-      <div>
-        <p>Album component goes here</p>
-      </div>
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
     );
   }
 }
